cd0edbb4e3919b70a4c4ccc443478226b2501e5a
Sirisha Dalasari <sirishadalasari2006@gmail.com>
2026-03-02 23:39:11 +0530

    generates dynamic index page using eta

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import * as pokiesData from "./public/data/poki_data.json" with {type:"json"}
+import * as pokiesData from "./public/data/poki_data.json" with {
+  type: "json",
+};
 import { createApp } from "./src/app.js";
 
-const main = () => {
+const main = (pokiesData) => {
   const app = createApp(pokiesData);
   Deno.serve(app.fetch);
-}
+};
 
-main()
+main(pokiesData.default);
--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import {Eta} from "eta"
 import { serveStatic } from "hono/deno";
-
-export const createApp = (pokiData) => {
+export const createApp = (pokiesData) => {
   const app = new Hono();
   app.use(logger());
   app.use(async (c, next) => {
-    c.set("pokiData", pokiData);
+    c.set("pokiesData", pokiesData);
     await next();
   });
-  app.get("*", serveStatic({ root: "public" }));
+  app.get("/", serveIndexPage);
+  app.get("*", serveStatic({root:'public'}));
   return app;
 };
 
+const getPokiTypes = (pokiesData) => {
+  const types = new Set();
+  pokiesData.forEach(pokiData => {
+    pokiData.types.forEach(type => {
+      types.add(type)
+    });
+  });
+  return [...types]
+}
+
+
+const serveIndexPage = (c) => {
+  const pokiesData = c.get("pokiesData");
+  const pokiTypes = getPokiTypes(pokiesData);
+  const eta = new  Eta({views:"public/templates"});
+  const indexPage = eta.render("./index.eta",{pokiTypes});
+  return c.html(indexPage);
+}
